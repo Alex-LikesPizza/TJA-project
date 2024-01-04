995f31e4991e9ff0019bd9b86eff1c6f791bb092
Alex-LikesPizza <alexandru13321@gmail.com>
2024-01-04 14:29:59 +0200

    main page progress, added footer

--- js/components/header.js
+++ js/components/header.js
@@ -3,14 +3,12 @@ const headerScroll = () => {
   const pageYOffset = window.scrollY;
 
   if(pageYOffset === 0){
-    header.style.position = "absolute";
     header.style.padding =  "1rem";
     header.style.height = "8rem";
   }
   else{
-    header.style.position = "sticky";
     header.style.padding =  "1rem";
     header.style.height = "6rem";
   }
 }
-setInterval(headerScroll, 10);
+window.addEventListener("scroll", headerScroll)
