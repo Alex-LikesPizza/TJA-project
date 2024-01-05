d888fc5befd92056a9e743604c1c2227d0366f5c
Alex-LikesPizza <alexandru13321@gmail.com>
2024-01-05 21:50:10 +0200

    added 'produse' search

--- js/components/header.js
+++ js/components/header.js
@@ -3,11 +3,13 @@ const headerScroll = () => {
   const pageYOffset = window.scrollY;
 
   if(pageYOffset === 0){
-    header.style.padding =  "1rem";
+    header.style.paddingTop =  "1rem";
+    header.style.paddingBottom =  "1rem";
     header.style.height = "8rem";
   }
   else{
-    header.style.padding =  "1rem";
+    header.style.paddingTop =  "0.2rem";
+    header.style.paddingBottom =  "0.2rem";
     header.style.height = "6rem";
   }
 }
