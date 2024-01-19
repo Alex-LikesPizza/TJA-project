fc4c9842ea0d5db5375f23c09c21787bcfe42772
Alex-LikesPizza <alexandru13321@gmail.com>
2024-01-19 20:31:30 +0200

    improvement tweaks

--- js/components/header.js
+++ js/components/header.js
@@ -6,11 +6,13 @@ const headerScroll = () => {
     header.style.paddingTop =  "1rem";
     header.style.paddingBottom =  "1rem";
     header.style.height = "8rem";
+    header.style.fontSize = "1.6rem"
   }
   else{
     header.style.paddingTop =  "0.2rem";
     header.style.paddingBottom =  "0.2rem";
     header.style.height = "5rem";
+    header.style.fontSize = "1.4rem"
   }
 }
 window.addEventListener("scroll", headerScroll)
