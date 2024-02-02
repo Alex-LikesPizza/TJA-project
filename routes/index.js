2a134640aa0a659ab7852aad8a9490ef89cf8b64
Alex-LikesPizza <alexandru13321@gmail.com>
2024-02-02 16:57:48 +0200

    server opens index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const route = express.Router();
 
-route.use("/", (req, res, next) =>{
-  console.log("a");
-})
+const distPath = path.join(__dirname, '..', 'dist');
+const homePath = path.join(distPath, "index.html");
+route.use("/home", (req, res, next) =>{
+  const fileBuffer = fs.readFileSync(homePath);
+  res.setHeader('Content-Type', 'text/html');
+  res.send(fileBuffer)
+});
 
 module.exports = route;
